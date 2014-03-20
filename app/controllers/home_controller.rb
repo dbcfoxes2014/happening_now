bac18d5075fc85295959b0918483906512ae45ea
To-mos <tomcatd10@gmail.com>
2014-03-20 11:27:09 -0500

    creating search routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
-  	
+
+  end
+
+  def search
+    @media = Instagram.media_popular
+    puts @media
+    # redirect_to :home
+  end
+
+  def show
+
   end
 end
