6eae0f4000efe2f62fb8b72a28e178d162e98634
Joe Wilmoth <jbwilmoth@gmail.com>
2014-03-20 16:46:04 -0500

    images are now in a grid

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	// $("a.fancybox").fancybox();
 
 	$("a[href$='.mp4'], a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").attr('rel', 'gallery').fancybox();//makes every image link on the site fancy!
-	$(".fancybox").attr('rel', 'gallery').fancybox();
+	
 	$("#video-thumbnail").on('click',function(){
 	    $.fancybox({
 	    'width'             : '75%',
