c49d445eed309438b448b3d14d103fdcba7853c9
Joe Wilmoth <jbwilmoth@gmail.com>
2014-03-20 15:07:52 -0500

    fancybox now works with videojs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   def index
   	@user = current_user
   end
+
+  def video 
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ FreeCandy::Application.routes.draw do
   devise_for :users
   resources :users, :only => :show
   resources :dan
+  match 'video', to: 'home#video', via: :get
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
