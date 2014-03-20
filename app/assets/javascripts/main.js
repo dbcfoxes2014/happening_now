0b4813818cea42e54c6477954950138b4acefa73
Joe Wilmoth <jbwilmoth@gmail.com>
2014-03-20 18:33:47 -0500

    going to merge the master into this

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 	// $("a.fancybox").fancybox();
 
-	$("a[href$='.mp4'], a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").attr('rel', 'gallery').fancybox();//makes every image link on the site fancy!
-	
-	$("#video-thumbnail").on('click',function(){
+	$("a[href$='.mp4'], a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").attr('rel', 'gallery').fancybox();//makes every image link on the site fancy
+	$(".video-thumbnail").on('click',function(){
 	    $.fancybox({
 	    'width'             : '75%',
 	    'height'            : '75%',
