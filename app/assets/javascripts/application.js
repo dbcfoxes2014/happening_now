6eb7d6409bbb66193059d07d927d866c47c30846
Joe Wilmoth <jbwilmoth@gmail.com>
2014-03-20 13:06:53 -0500

    lightbox is in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require lightbox-2.6.min.js
 //= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 FreeCandy::Application.routes.draw do
   devise_for :users
   resources :users, :only => :show
+  resources :dan
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
