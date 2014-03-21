909236dacad121a9f9275a5cf996948e67f7e3c7
Chris Prater <cprater23@gmail.com>
2014-03-21 17:58:07 -0500

    pulling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -30,26 +30,26 @@ class HomeController < ApplicationController
       @media = grab_all_media(@search_content)  
     end 
     
-    # binding.pry
+    
     render :display
   end
 
   def next
-    @media = Instagram.tag_recent_media(session[:tag], {MIN_ID: params[:min_id], MAX_ID: params[:max_id]})
+    # @message = params[:tag]
+    # @media = Instagram.tag_recent_media("test", {MAX_ID: params[:max_id]})
+    # @media = Instagram::Request.get("https://api.instagram.com/v1/tags/test/media")
     render :display
   end
 
 
   def back
-
+    @message = params[:tag]
+    @media = Instagram.tag_recent_media("test", {MIN_ID: params[:min_id]})
+    render :display
   end
-  
-  def save_media
-    # binding.pry
-    session[:media_url] ||= []
-    session[:media_url].push(params[:media_url])
-    p session[:media_url].first
-    p "--------------------- dan new here -----------"
 
+  def save_media
+    session[:media_url] ||= []
+    session[:media_url].push(params[:media_url])  
   end
 end
