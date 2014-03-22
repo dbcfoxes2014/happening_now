a4628b68afab212a24f81d98336f059f97852b1b
Chris Prater <cprater23@gmail.com>
2014-03-21 19:43:35 -0500

    pulling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -34,20 +34,6 @@ class HomeController < ApplicationController
     render :display
   end
 
-  def next
-    # @message = params[:tag]
-    # @media = Instagram.tag_recent_media("test", {MAX_ID: params[:max_id]})
-    # @media = Instagram::Request.get("https://api.instagram.com/v1/tags/test/media")
-    # render :display
-  end
-
-
-  def back
-    # @message = params[:tag]
-    # @media = Instagram.tag_recent_media("test", {MIN_ID: params[:min_id]})
-    # render :display
-  end
-
   def save_media
     session[:media_url] ||= []
     session[:media_url].push(params[:media_url])
