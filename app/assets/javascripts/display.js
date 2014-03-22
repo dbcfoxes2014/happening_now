f6eae1e83bdecffa1c6c163b43455acc98232757
Chris Prater <cprater23@gmail.com>
2014-03-22 17:25:36 -0500

    pulling

--- app/assets/javascripts/display.js
+++ app/assets/javascripts/display.js
@@ -1,22 +1,45 @@
 $(document).ready(function(){
 
-	//Display media without refreshing the pages
+	//Display Popular media without refreshing the pages
 	$(document).on('click', '.popular-button', function(event){
 		event.preventDefault();
-
-		console.log("click")
 
 		$.ajax({
 			url: 'popular',
 			type: 'get',
-
 				success: function(serverResponse){
 					$('.container').html(serverResponse);
-
 				}
 		});
 	});
 
+	//Display Search media without refreshing page
+	$(document).on('submit', '.search-form', function(event){
+		event.preventDefault();
+
+		$.ajax({
+			url: 'search',
+			data: $('form').serialize(),
+			type: 'get', 
+				success: function(serverResponse){
+					$('.container').html(serverResponse);
+				}
+		})
+	});
+
+	//Display similar media without refreshing page
+	$(document).on('click', '.sim-media', function(event){
+		event.preventDefault();
+
+		$.ajax({
+			url: 'search',
+			data: {var search_data: $(this).text()},
+			type: 'get',
+				success: function(serverResponse){
+					$('.container').html(serverResponse);
+				}
+		})
+	});
 });
 
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,6 +14,7 @@ class HomeController < ApplicationController
   end
 
   def search
+    # binding.pry
     if params[:search_data] == ""
       flash[:alert] = "Enter something to search"
       redirect_to :root and return
@@ -23,16 +24,16 @@ class HomeController < ApplicationController
     similar_tags = find_similar_tags(@search_content)
     @message = "Search Results for #{@search_content}"
 
-    if params[:commit] == "Search Images"
-      @media = grab_select_media(@search_content, "image")
-    elsif params[:commit] == "Search Videos"
-      @similar_media = grab_select_media(similar_tags, "video").sample(4)
-      @media = grab_select_media(@search_content, "video")
-    else
+    # if params[:commit] == "Search Images"
+    #   @media = grab_select_media(@search_content, "image")
+    # elsif params[:commit] == "Search Videos"
+    #   @similar_media = grab_select_media(similar_tags, "video").sample(4)
+    #   @media = grab_select_media(@search_content, "video")
+    # else
       @similar_media = grab_all_media(similar_tags).sample(4)
       @media = grab_all_media(@search_content)  
-    end
-    render :display
+    # end
+    render partial: 'home/display', layout: false
   end
 
   def save_media
@@ -49,5 +50,5 @@ class HomeController < ApplicationController
     search_terms = ["puppies", "dogs", "cats", "airplanes", "skateboarding", "dbc", "water", "fly"]
     @content = grab_select_media(search_terms, "video")
     render partial: "debug_grab_test_urls"
-   end
+  end
 end
