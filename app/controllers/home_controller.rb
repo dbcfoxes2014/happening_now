49a6aceb24a591775a889260f92222d253005551
Chris Prater <cprater23@gmail.com>
2014-03-23 15:23:28 -0500

    pulling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,22 +17,29 @@ class HomeController < ApplicationController
       redirect_to :root and return
     end
 
+    
+
     @search_content = seperate_values(params[:search_data], ' ')
     similar_tags = find_similar_tags(@search_content)
     @message = "Search Results for #{@search_content}"
 
-    # if params[:commit] == "Search Images"
-    #   @media = grab_select_media(@search_content, "image")
-    # elsif params[:commit] == "Search Videos"
-    #   @similar_media = grab_select_media(similar_tags, "video").sample(4)
-    #   @media = grab_select_media(@search_content, "video")
-    # else
+    if params[:search][:images] == "1" && params[:search][:videos] == "1"
       @similar_media = grab_all_media(similar_tags).sample(4)
       @media = grab_all_media(@search_content)
+    elsif params[:search][:images] == "1"
+      @similar_media = grab_select_media(similar_tags, "image").sample(4)
+      @media = grab_select_media(@search_content, "image")
+    elsif params[:search][:videos] == "1"
+      @similar_media = grab_select_media(similar_tags, "video").sample(4)
+      @media = grab_select_media(@search_content, "video")
+    else
+      @similar_media = grab_all_media(similar_tags).sample(4)
+      @media = grab_all_media(@search_content)
+    end
+
       if current_user
         @flagged_media = FlaggedContent.where(user_id: current_user.id).pluck(:url)
       end
-    # end
   end
 
   def save_media
