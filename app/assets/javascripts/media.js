6bfb930e243585762aaa7e5a6237d28520de94b9
Chris Prater <cprater23@gmail.com>
2014-03-23 22:10:54 -0500

    hover over pics to get links to user media

--- app/assets/javascripts/media.js
+++ app/assets/javascripts/media.js
@@ -1,8 +1,16 @@
 $(function(){
-		console.log("yo")
 
 	$(document).on('mouseenter', '.thumbnail_object', function(){
-		$(this).find('pic-username').css('visibility', 'hidden')
-
+		var link = $(this).find('.pic-username')
+		link.removeClass('hide-thumbnail')
+		// link.on('mouseenter', function(){
+		// 	$(this).css('visibility', 'visible')
+		// });
 	});
+
+	$(document).on('mouseleave', '.thumbnail_object', function(){
+		$(this).find('.pic-username').addClass('hide-thumbnail')
+	});
+
+
 });	
