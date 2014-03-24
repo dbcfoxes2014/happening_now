b74558530fb892fc62c124af70f39b055e0c84ff
Joe Wilmoth <jbwilmoth@gmail.com>
2014-03-24 17:51:48 -0500

    Fixed ruby version and gem spec versions.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,33 @@
-require 'bundler/capistrano'
-
 set :application, "free_candy"
 set :repository,  "https://github.com/dbcfoxes2014/free_candy"
-
 set :deploy_to, "/var/www/#{application}"
 set :scm, :git
 set :branch, "master"
-
 set :user, "deployer"
-
+set :group, "deployers"
 set :rails_env, "production"
-
 set :deploy_via, :copy
-
 set :ssh_options, { :forward_agent => true, :port => 1026 }
-
 set :keep_releases, 5
-
 default_run_options[:pty] = true
-
 server "107.170.118.223", :app, :web, :db, :primary => true
 
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
+	task :start do ; end
+	task :stop do ; end
+
 	desc "Symlink shared config files"
 	task :symlink_config_files do
-		run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
+		run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml && #{ try_sudo } ln -s #{ deploy_to }/shared/config/instagram.yml #{ current_path }/config/instagram.yml"
 	end
+  
+  desc "Precompile assets after deploy"
+  task :precompile_assets do
+  	run <<-CMD
+  	  cd #{ current_path } &&
+  	  #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
+  	CMD
+  end
 
 	desc "Restart Passenger app"
 	task :restart do
@@ -35,7 +36,5 @@ namespace :deploy do
 end
 
 after "deploy", "deploy:symlink_config_files"
-
 after "deploy", "deploy:restart"
-
 after "deploy", "deploy:cleanup"
