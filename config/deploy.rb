a3f3eee4e28ff16f35f2c943dfb1fc819548b733
Joe Wilmoth <jbwilmoth@gmail.com>
2014-03-23 22:35:19 -0500

    Removed separate roles from capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,11 +20,6 @@ default_run_options[:pty] = true
 
 server "107.170.118.223", :app, :web, :db, :primary => true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
-
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
