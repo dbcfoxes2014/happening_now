d9f0a6e5e8c80f0c180ea28d98054d9ddfbb46c5
Joe Wilmoth <jbwilmoth@gmail.com>
2014-03-23 22:27:23 -0500

    Set capistrano to copy the repo to the server when deploying.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,8 @@ set :use_sudo, false
 
 set :rails_env, "production"
 
+set :deploy_via, :copy
+
 role :web, "your web-server here"                          # Your HTTP server, Apache/etc
 role :app, "your app-server here"                          # This may be the same as your `Web` server
 role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
