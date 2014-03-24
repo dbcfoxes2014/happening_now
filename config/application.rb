2258d089fdb905155edde921bacc5da16e5c9d3c
jdubnicek <jdubnicek@gmail.com>
2014-03-24 13:54:34 -0500

    EventBrite SO CLOSE MORESO

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,8 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'json'
 require 'open-uri'
+require 'eventbrite-client'
+require 'eventbrite'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
