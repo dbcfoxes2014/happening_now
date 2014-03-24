5cebb0d52e331d717d8ec26f02a1ba456e24aa8a
jdubnicek <jdubnicek@gmail.com>
2014-03-24 13:58:54 -0500

    EventBrite SO CLOSE MORESO ALMOST

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,18 +2,13 @@
 class EventsController < ApplicationController
   respond_to :json
 
-	
 	#API Request
-
 	#initialize our http object, for contacting the API
 	http = Net::HTTP.new('www.eventbrite.com', 443)
 	http.use_ssl=true
 
-	#make API request
-
-
 	#make our API request - 
-	response = http.request_get( URI.escape('https://www.eventbrite.com/json/event_search?keywords=popular'))
+	response = eb_client.event_search({ "Last Week" })
 
 	#parse our JSON response data...
 	events_list = JSON.parse(response.body)
