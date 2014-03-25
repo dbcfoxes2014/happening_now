2533e3dbfa9ad9d6f837aaef3a2f8f0c2217a74c
To-mos <tomcatd10@gmail.com>
2014-03-24 19:50:07 -0500

    merging master

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -16,14 +16,15 @@ class EditorController < ApplicationController
     if(params[:query])
       case params[:query]
         when 'slotAvaliable'
-          response.merge!({status: 'renderReady',slot: rand(200)})
+          response.merge!({status: 'renderReady',job_id: rand(200)})
         when 'renderTime'
-          response.merge!({status: 'renderTime',time: 12})
+          #respond with the job's current background status
+          response.merge!({status: job_is_complete(params[:job_id]) ? "done" : "working",time: 12})
         else
-
+          response = {status: 'Command Not Recognized'}
         end
     else
-      response = {status: 'Command Not Recognized'}
+      response = {status: 'Query is empty'}
     end
     render json: response
   end
@@ -41,18 +42,20 @@ class EditorController < ApplicationController
           grabPicURLs(params[:urls])
           response.merge!({status: 'photosDownloaded'})
         when 'startVideoRender'
-          puts "Starting Render on slot: #{params[:slot]}"
-          response.merge!({status: 'renderVideoStart',slot: rand(200)})
-          renderMovies()
-          response.merge!({status: 'renderVideoFinished',slot: rand(200)})
+          response.merge!({status: 'renderVideoStart',job_id: rand(200)})
+          job_id = RenderWorker.perform_async(session[:videos],'movie')
+          RenderQueue.create(user_id: current_user.id, job_id: job_id)
+          puts "Starting Render on job_id: #{job_id}"
+          response.merge!({status: 'renderVideoFinished',job_id: job_id})
         when 'startPhotoRender'
-          puts "Starting Render on slot: #{params[:slot]}"
-          response.merge!({status: 'renderPhotoStart',slot: rand(200)})
-          renderPhotos()
-          response.merge!({status: 'renderPhotoFinished',slot: rand(200)})
+          response.merge!({status: 'renderPhotoStart',job_id: rand(200)})
+          job_id = RenderWorker.perform_async(session[:photos],'slideshow')
+          RenderQueue.create(user_id: current_user.id, job_id: job_id)
+          puts "Starting Render on job_id: #{job_id}"
+          response.merge!({status: 'renderPhotoFinished',job_id: job_id})
         when 'stopRender'
-          puts "Stopping Render on slot: #{params[:slot]}"
-          response.merge!({status: 'renderStop',slot: rand(200)})
+          puts "Stopping Render on job_id: #{params[:slot]}"
+          response.merge!({status: 'renderStop',job_id: rand(200)})
         when 'verbocity'
           dir = params[:direction]
           if dir == "+"
@@ -62,9 +65,11 @@ class EditorController < ApplicationController
             puts "Decreasing Verbocity"
             response.merge!({status: 'verbChange',level: 0})
           end
+        else
+          response = {status: 'Command Not Recognized'}
         end
     else
-      response = {status: 'Command Not Recognized'}
+      response = {status: 'Query is empty'}
     end
     render json: response
   end
@@ -83,36 +88,6 @@ private
     session[:videos] = movie_files
   end
 
-  def renderMovies
-    movie_ffmpeg = Array.new
-    #concate movies via transcoding
-    session[:videos].map do |mov|
-      movie_ffmpeg << FFMPEG::Movie.new("public/data/#{mov}")
-    end
-    args = movie_ffmpeg[1..-1].map{ |mov| "-i " + mov.path }.join(" ")
-    puts "Command: #{args}"
-    #if(movie_ffmpeg.empty?)
-
-    movie_name = nil
-    if (Video.all.length == 0)
-      movie_name = "data/1.mp4"
-      screenshot_name = "data/1.jpg"
-    else
-      movie_name = "data/#{Video.last.id + 1}.mp4"
-      screenshot_name = "data/#{Video.last.id + 1}.jpg"
-    end
-
-    movie_ffmpeg[0].transcode(
-        "public/#{movie_name}",
-        "#{args} -s 640x640 -filter_complex concat=n=#{movie_ffmpeg.size}:v=1:a=1 -threads 4 -strict -2",
-        'movie'
-    )
-
-    movie_ffmpeg[0].screenshot("public/#{screenshot_name}", seek_time: 5, resolution: '256x256')
-
-    Video.create(user_id: current_user.id, title: "we shouldnt have a title", file_path: "#{movie_name}", thumbnail_path: "#{screenshot_name}").save
-  end
-
   def grabPicURLs(urls)
     photo_files = Array.new
     urls.map do |url|
@@ -125,36 +100,9 @@ private
     session[:photos] = photo_files
   end
 
-  def renderPhotos
-    photo_ffmpeg = Array.new
-    #concate movies via transcoding
-    session[:photos].map do |pic|
-      photo_ffmpeg << FFMPEG::Movie.new("public/data/*.jpg")
-    end
-    args = photo_ffmpeg[1..-1].map{ |pic| "-i " + pic.path }.join(" ")
-    #if(movie_ffmpeg.empty?)
-
-    slideshow_name = nil
-    if (Video.all.length == 0)
-      slidheshow_name = "data/img1.mp4"
-      thumbnail_name = "data/thumb1.jpg"
-    else
-      slideshow_name = "data/img#{Video.last.id + 1}.mp4"
-      thumbnail_name = "data/thumb#{Video.last.id + 1}.jpg"
-    end
-    puts "Command: #{args} -filter_complex concat=n=#{photo_ffmpeg.size}:v=1:a=0 -r 1/5"
-    #works on terminal
-    #ffmpeg -r 1/5 -pattern_type glob -i 'public/data/*.jpg' -c:v libx264 public/data/out.mp4
-    #ffmpeg -i 'public/data/img2cd06c_8.jpg' -i public/data/img670ba3_8.jpg -filter_complex concat=n=2:v=1:a=0 -r 1/5 -c:v libx264 public/data/out.mp4
-    photo_ffmpeg[0].transcode(
-        "public/#{slideshow_name}",
-        "#{args} -filter_complex concat=n=#{photo_ffmpeg.size}:v=1:a=0 -r 1/5",
-        'slideshow'
-
-    )
-
-    #photo_ffmpeg[0].screenshot("public/#{thumbnail_name}", seek_time: 5, resolution: '256x256')
-
-    Video.create(user_id: current_user.id, title: "we shouldnt have a title", file_path: "#{slideshow_name}", thumbnail_path: "#{thumbnail_name}").save
+  def returnUserJobs(user)
+    user_found = RenderQueue.where(user_id: user).all
+    #unless user_found.nil? return user_found.map{|index| index.job_id}
   end
+
 end
